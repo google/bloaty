a0cdadedc562a34b1c817bfa5594d28e8e626313
Josh Haberman <jhaberman@gmail.com>
2016-05-24 14:52:41 -0400

    Broken dominator algorithm.

--- bloaty.cc
+++ bloaty.cc
@@ -12,13 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <fstream>
+#include <iostream>
 #include <map>
-#include <unordered_map>
 #include <set>
 #include <string>
-#include <iostream>
+#include <unordered_map>
 #include <vector>
+
 #include <stdlib.h>
+
 #include "re2/re2.h"
 #include <assert.h>
 
@@ -113,6 +116,22 @@ class NameStripper {
   std::string storage_;
 };
 
+class Demangler {
+ public:
+  std::string Demangle(const std::string& symbol) {
+    // TODO: optimize to keep a pipe open so we don't exec c++filt separately for
+    // every single symbol.
+    std::string last;
+    for ( auto& line : ReadLinesFromPipe("c++filt " + symbol) ) {
+      last = line;
+    }
+    if (!last.empty()) {
+      last.erase(last.size() - 1);
+    }
+    return last;
+  }
+};
+
 
 /** Program data structures ***************************************************/
 
@@ -129,12 +148,31 @@ struct File {
 };
 
 struct Object {
-  Object(const std::string& name_) : name(name_), size(0), file(NULL) {}
+  Object(const std::string& name_) :
+      name(name_),
+      size(0),
+      in_ref_count(0),
+      seen_count(0),
+      file(NULL) {}
+
   // Declared name of the symbol.
   std::string name;
 
+  // Name possibly put through c++filt, but also reduced to remove overloads if possible.
+  std::string pretty_name;
+
+  void SetSize(size_t size_) {
+    size = size_;
+    weight = size_;
+  }
+
   uintptr_t vmaddr;
   size_t size;
+  size_t weight;
+  uint32_t in_ref_count;
+  uint32_t seen_count;
+  uint32_t first_id;
+  std::vector<Object*> first_stack;
 
   // Whether this is from a data section (rather than code).
   // When this is true we will scan the object for pointers.
@@ -147,20 +185,98 @@ struct Object {
   std::set<Object*> refs;
 };
 
+class Stacks {
+ public:
+  void Insert(Object* obj, uint32_t id) {
+    stacks_[id].push_back(obj);
+    stack_.push_back(obj);
+    stack_set_.insert(obj);
+  }
+
+  bool IsOnStack(Object* obj) {
+    return stack_set_.find(obj) != stack_set_.end();
+  }
+
+  void Remove(Object* obj, uint32_t id) {
+    assert(stacks_[id].back() == obj);
+    assert(stack_.back() == obj);
+    stacks_[id].pop_back();
+    stack_.pop_back();
+    stack_set_.erase(obj);
+  }
+
+  Object* FindAncestor(Object* node) {
+    std::cerr << "Finding ancestor for " << node->name << "\n";
+    // Find ancestor in the more efficient way.
+    uint32_t id = node->first_id;
+    auto it = stacks_.upper_bound(id);
+    assert(it != stacks_.begin());
+    --it;
+    while (it->second.empty()) {
+      assert(it != stacks_.begin());
+      --it;
+    }
+    Object* ancestor = it->second.back();
+
+    std::cerr << "The fast way yielded " << ancestor->name << "\n";
+
+    // Find ancestor in the safer way.
+    for (int i = node->first_stack.size() - 1; i >= 0; i--) {
+      if (stack_set_.find(node->first_stack[i]) != stack_set_.end()) {
+        std::cerr << "The safe way yielded " << node->first_stack[i]->name << "\n";
+        assert(node->first_stack[i] == ancestor);
+        break;
+      }
+    }
+
+    return ancestor;
+  }
+
+  Object* GetTop() {
+    assert(!stack_.empty());
+    return stack_.back();
+  }
+
+  void StoreFirstStack(Object* obj, uint32_t id) {
+    obj->first_id = id;
+    obj->first_stack = stack_;
+  }
+
+ private:
+  std::map<uint32_t, std::vector<Object*>> stacks_;
+  std::vector<Object*> stack_;
+  std::set<Object*> stack_set_;
+};
+
 class Program {
  public:
   Object* AddObject(const std::string& name, uintptr_t vmaddr, size_t size, bool data) {
-    std::cout << "added name: '" << name << "'\n";
-    if (stripper_.StripName(name)) {
-      stripped_names_[stripper_.stripped()]++;
-    }
 
     auto pair = objects_.emplace(name, name);
     Object* ret = &pair.first->second;
     ret->vmaddr = vmaddr;
-    ret->size = size;
+    ret->SetSize(size);
     ret->data = data;
+    ret->name = name;
     objects_by_addr_[vmaddr] = ret;
+
+    auto demangled = demangler_.Demangle(name);
+    if (stripper_.StripName(demangled)) {
+      auto it = stripped_pretty_names_.find(stripper_.stripped());
+      if (it == stripped_pretty_names_.end()) {
+        stripped_pretty_names_[stripper_.stripped()] = ret;
+        ret->pretty_name = stripper_.stripped();
+      } else {
+        ret->pretty_name = demangled;
+        if (it->second) {
+          it->second->pretty_name = demangler_.Demangle(it->second->name);
+          it->second = NULL;
+        }
+      }
+    } else {
+      ret->pretty_name = demangled;
+    }
+
     return ret;
   }
 
@@ -173,7 +289,9 @@ class Program {
     if (it != objects_by_addr_.end()) {
       Object* to = it->second;
       if (to) {
-        from->refs.insert(to);
+        if (from->refs.insert(to).second) {
+          to->in_ref_count++;
+        }
         if (from->file && to->file) {
           from->file->refs.insert(to->file);
         }
@@ -192,17 +310,157 @@ class Program {
 
   bool HasFiles() { return files_.size() > 0; }
 
-  const std::string& GetMaybeStrippedName(const std::string& name) {
-    if (stripper_.StripName(name) && stripped_names_[stripper_.stripped()] == 1) {
-      return stripper_.stripped();
-    } else {
-      return name;
-    }
-  }
-
   Object* FindFunctionByName(const std::string& name) {
     auto it = objects_.find(name);
     return it == objects_.end() ? NULL : &it->second;
+  }
+
+  uint32_t CalculateWeights(Object* node, uint32_t id, Stacks* stacks) {
+    if (node->seen_count++ == 0) {
+      stacks->Insert(node, id);
+      stacks->StoreFirstStack(node, id);
+      for ( auto& target : node->refs ) {
+        if (!stacks->IsOnStack(target)) {  // Break cycles for now.
+          id = CalculateWeights(target, id, stacks) + 1;
+        }
+      }
+      stacks->Remove(node, node->first_id);
+    }
+
+    if (node->in_ref_count == 0) {
+      // Nobody references us, so we don't add our weight to anything.
+      std::cerr << "Not adding weight from " << node->name << ", nobody references us.\n";
+    } else if (node->in_ref_count == 1) {
+      // We have exactly one caller, so just add ourself to our caller's weight.
+      std::cerr << "Adding weight from " << node->name << " to " << stacks->GetTop()->name << "\n";
+      stacks->GetTop()->weight += node->weight;
+    } else if (node->in_ref_count == node->seen_count) {
+      // We have multiple callers, find the lowest common ancestor by looking
+      // for the deepest node on our stack with a first_id <= to ours.
+      Object* ancestor = stacks->FindAncestor(node);
+      std::cerr << "Ancestor for " << node->name << " was " << ancestor->name << "\n";
+      ancestor->weight += node->weight;
+    } else {
+      std::cerr << "Skipping: " << node->name << " because we expect " << (node->in_ref_count - node->seen_count) << " more in edges.\n";
+    }
+
+    return id;
+  }
+
+  void PrintDotGraph(Object* obj, std::ofstream* out, std::set<Object*>* seen) {
+    if (!seen->insert(obj).second) {
+      return;
+    }
+
+    *out << "  \"" << obj->name << "\" [label=\"" << obj->pretty_name << "\\nsize: " << obj->size << "\\nweight: " << obj->weight << "\"];\n";
+
+    for ( auto& target : obj->refs ) {
+      //if (target->weight > 200) {
+        *out << "  \"" << obj->name << "\" -> \"" << target->name << "\";\n";
+        PrintDotGraph(target, out, seen);
+      //}
+    }
+  }
+
+  void PrintSymbolsByTransitiveWeight() {
+    Object* obj = FindFunctionByName("_main");
+    Stacks stacks;
+    CalculateWeights(obj, 0, &stacks);
+    /*
+
+    std::vector<Object*> object_list;
+    object_list.reserve(objects_.size());
+    for ( auto& pair : objects_ ) {
+      object_list.push_back(&pair.second);
+      assert(pair.first == pair.second.name);
+    }
+
+    std::sort(object_list.begin(), object_list.end(), [](Object* a, Object* b) {
+      return a->weight > b->weight;
+    });
+
+    for ( auto object : object_list) {
+      printf(" %7d %s\n", (int)object->weight, object->name.c_str());
+    }
+    */
+
+    std::ofstream out("graph.dot");
+    out << "digraph weights {\n";
+    /*
+    for ( auto object : object_list) {
+      out << "  \"" << object->name << "\"\n";
+    }
+    */
+    std::set<Object*> seen;
+    PrintDotGraph(obj, &out, &seen);
+    out << "}\n";
+  }
+
+  void GC(Object* obj, std::set<Object*>* garbage) {
+    if (garbage->erase(obj) != 1) {
+      return;
+    }
+
+    std::cerr << "Reached obj: " << obj->name << "\n";
+
+    for ( auto& child : obj->refs ) {
+      GC(child, garbage);
+    }
+  }
+
+  void GCFiles(File* file, std::set<File*>* garbage) {
+    if (garbage->erase(file) != 1) {
+      return;
+    }
+
+    std::cerr << "Reached file: " << file->name << "\n";
+
+    for ( auto& child : file->refs ) {
+      GCFiles(child, garbage);
+    }
+  }
+
+  void PrintGarbage() {
+    std::set<Object*> garbage;
+
+    for ( auto& pair : objects_ ) {
+      garbage.insert(&pair.second);
+    }
+
+    Object* obj = FindFunctionByName("_main");
+
+    if (!obj) {
+      std::cerr << "Error: couldn't find main function.\n";
+      exit(1);
+    }
+
+    GC(obj, &garbage);
+
+    std::cerr << "Total objects: " << objects_.size() << "\n";
+    std::cerr << "Garbage objects: " << garbage.size() << "\n";
+
+    for ( auto& obj : garbage ) {
+      std::cerr << "Garbage obj: " << obj->name << "\n";
+      for ( auto& ref : obj->refs ) {
+        ref->in_ref_count--;
+      }
+    }
+
+    if (obj->file) {
+      std::set<File*> garbage_files;
+      for ( auto& pair : files_ ) {
+        garbage_files.insert(&pair.second);
+      }
+
+      GCFiles(obj->file, &garbage_files);
+
+      std::cerr << "Total files: " << files_.size() << "\n";
+      std::cerr << "Garbage files: " << garbage_files.size() << "\n";
+
+      for ( auto& file : garbage_files ) {
+        std::cerr << "Garbage file: " << file->name << "\n";
+      }
+    }
   }
 
   void PrintSymbols() {
@@ -225,7 +483,7 @@ class Program {
     for ( auto object : object_list) {
       size_t size = object->size;
       cumulative += size;
-      const std::string& name = GetMaybeStrippedName(object->name);
+      const std::string& name = object->pretty_name;
       printf("%5.1f%% %5.1f%%  %6d %s\n", size / total * 100, cumulative / total * 100, (int)size, name.c_str());
     }
 
@@ -251,7 +509,7 @@ class Program {
     for ( auto file : file_list) {
       size_t size = file->source_line_weight;
       cumulative += size;
-      const std::string& name = GetMaybeStrippedName(file->name);
+      const std::string& name = file->name;
       printf("%5.1f%% %5.1f%%  %6d %s\n", size / total * 100, cumulative / total * 100, (int)size, name.c_str());
     }
 
@@ -264,12 +522,11 @@ class Program {
   // Objects, indexed by name.
   std::unordered_map<std::string, Object> objects_;
   std::unordered_map<uintptr_t, Object*> objects_by_addr_;
-
-  // C++ stripped names with a count of each one.
-  std::unordered_map<std::string, int> stripped_names_;
+  std::unordered_map<std::string, Object*> stripped_pretty_names_;
 
 
   NameStripper stripper_;
+  Demangler demangler_;
 };
 
 bool StartsWith(const std::string& haystack, const std::string& needle) {
@@ -277,9 +534,10 @@ bool StartsWith(const std::string& haystack, const std::string& needle) {
 }
 
 void ParseMachOSymbols(const std::string& filename, Program* program) {
-  std::string cmd = std::string("symbols -fullSourcePath ") + filename;
+  std::string cmd = std::string("symbols -fullSourcePath -noDemangling ") + filename;
 
   // [16 spaces]0x00000001000009e0 (  0x3297) run_tests [FUNC, EXT, LENGTH, NameNList, MangledNameNList, Merged, NList, Dwarf, FunctionStarts]
+  // [16 spaces]0x00000001000015a0 (     0x9) __ZN10LineReader5beginEv [FUNC, EXT, LENGTH, NameNList, MangledNameNList, Merged, NList, Dwarf, FunctionStarts]
   RE2 pattern1(R"(^\s{16}0x([0-9a-f]+) \(\s*0x([0-9a-f]+)\) (.+) \[((?:FUNC)?))");
 
   // [20 spaces]0x00000001000009e0 (    0x21) tests/test_def.c:471
@@ -315,7 +573,7 @@ void ParseMachOSymbols(const std::string& filename, Program* program) {
 }
 
 void ParseMachODisassembly(const std::string& filename, Program* program) {
-  std::string cmd = std::string("otool -tV ") + filename + " | c++filt";
+  std::string cmd = std::string("otool -tV ") + filename;
 
   // ReadLinesFromPipe(std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char> > const&):
   RE2 func_pattern(R"(^([a-zA-Z_].*):)");
@@ -337,7 +595,6 @@ void ParseMachODisassembly(const std::string& filename, Program* program) {
   std::string name;
 
   for ( auto& line : ReadLinesFromPipe(cmd) ) {
-    std::cout << line;
     size_t addr;
     if (lea_offset) {
       if (RE2::PartialMatch(line, addr_pattern, RE2::Hex(&addr))) {
@@ -349,12 +606,13 @@ void ParseMachODisassembly(const std::string& filename, Program* program) {
 
     if (RE2::PartialMatch(line, leaq_pattern, RE2::Hex(&lea_offset))) {
       // Next line will find the actual address.
+      /*
       if (current_function) {
         std::cout << "Found lea in function; " << current_function->name << "\n";
       } else {
         std::cout << "Found lea but no current function!  Name: " << current_function_name;
       }
-      std::cout << line;
+      */
     } else if (RE2::PartialMatch(line, func_pattern, &current_function_name)) {
       current_function = program->FindFunctionByName(current_function_name);
 
@@ -363,6 +621,10 @@ void ParseMachODisassembly(const std::string& filename, Program* program) {
       }
 
       current_function = program->FindFunctionByName(current_function_name);
+
+      if (!current_function) {
+        std::cerr << "Couldn't find function for name: " << current_function_name << "\n";
+      }
     } else if (RE2::PartialMatch(line, call_pattern, &name) ||
                RE2::PartialMatch(line, leaq_sym_pattern, &name)) {
       while (name[name.size() - 1] == ' ') {
@@ -382,7 +644,9 @@ void ParseMachODisassembly(const std::string& filename, Program* program) {
         std::cerr << "Whoops!  Couldn't find a function entry for: '" << name << "'\n";
       }
 
-      current_function->refs.insert(target_function);
+      if (current_function->refs.insert(target_function).second) {
+        target_function->in_ref_count++;
+      }
 
       if (current_function->file != NULL && target_function->file != NULL) {
         current_function->file->refs.insert(target_function->file);
@@ -431,7 +695,8 @@ long GetDataSegmentDelta(const std::string& filename) {
 }
 
 void ParseVTables(const std::string& filename, Program* program) {
-  long delta = GetDataSegmentDelta(filename);
+  bool computed_delta = false;
+  long delta;
 
   FILE* f = fopen(filename.c_str(), "rb");
 
@@ -439,6 +704,11 @@ void ParseVTables(const std::string& filename, Program* program) {
     Object* obj = &pair.second;
     if (!obj->data) {
       continue;
+    }
+
+    if (!computed_delta) {
+      delta = GetDataSegmentDelta(filename);
+      computed_delta = true;
     }
 
     for (size_t i = 0; i < obj->size; i += sizeof(uintptr_t)) {
@@ -450,30 +720,6 @@ void ParseVTables(const std::string& filename, Program* program) {
   }
 
   fclose(f);
-}
-
-void GC(Object* obj, std::set<Object*>* garbage) {
-  if (garbage->erase(obj) != 1) {
-    return;
-  }
-
-  std::cerr << "Reached obj: " << obj->name << "\n";
-
-  for ( auto& child : obj->refs ) {
-    GC(child, garbage);
-  }
-}
-
-void GCFiles(File* file, std::set<File*>* garbage) {
-  if (garbage->erase(file) != 1) {
-    return;
-  }
-
-  std::cerr << "Reached file: " << file->name << "\n";
-
-  for ( auto& child : file->refs ) {
-    GCFiles(child, garbage);
-  }
 }
 
 int main(int argc, char *argv[]) {
@@ -491,44 +737,6 @@ int main(int argc, char *argv[]) {
     std::cerr << "Warning: no debug information present.\n";
   }
 
-  //program.PrintFiles();
-  //program.PrintSymbols();
-  //
-  std::set<Object*> garbage;
-
-  for ( auto& pair : program.objects_ ) {
-    garbage.insert(&pair.second);
-  }
-
-  Object* obj = program.FindFunctionByName("main");
-
-  if (!obj) {
-    std::cerr << "Error: couldn't find main function.\n";
-    exit(1);
-  }
-
-  GC(obj, &garbage);
-
-  std::cerr << "Total objects: " << program.objects_.size() << "\n";
-  std::cerr << "Garbage objects: " << garbage.size() << "\n";
-
-  for ( auto& obj : garbage ) {
-    std::cerr << "Garbage obj: " << obj->name << "\n";
-  }
-
-  if (obj->file) {
-    std::set<File*> garbage_files;
-    for ( auto& pair : program.files_ ) {
-      garbage_files.insert(&pair.second);
-    }
-
-    GCFiles(obj->file, &garbage_files);
-
-    std::cerr << "Total files: " << program.files_.size() << "\n";
-    std::cerr << "Garbage files: " << garbage_files.size() << "\n";
-
-    for ( auto& file : garbage_files ) {
-      std::cerr << "Garbage file: " << file->name << "\n";
-    }
-  }
+  program.PrintGarbage();
+  program.PrintSymbolsByTransitiveWeight();
 }
