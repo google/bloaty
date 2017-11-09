93b664c9197af3cbba76cbd41e845ce3e820954c
Josh Haberman <jhaberman@gmail.com>
2017-11-08 19:57:52 -0800

    Added missing <atomic> include needed on Linux.

--- src/bloaty.cc
+++ src/bloaty.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <atomic>
 #include <cmath>
 #include <fstream>
 #include <iostream>
