5a53d5ba49979395ca01b610501a503c90203fc6
brandonvu12 <bvu106038@gmail.com>
2022-07-12 17:25:43 +0000

    Remove Comment.

--- src/bloaty.h
+++ src/bloaty.h
@@ -357,7 +357,7 @@ struct RollupRow {
   // The size of this row in a diff base.  Same as `size` for non-diff.
   DomainSizes old_size = {0, 0};
   // The current size of this row (this is not the same as `size` in the case
-  // of a diff -- `size` is the delta). -1 if non-diff.
+  // of a diff -- `size` is the delta).
   DomainSizes new_size = {0, 0};
   
   std::vector<RollupRow> sorted_children;
